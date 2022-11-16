9fda0371be0ee40120f9258173b8877afacba27f
kflinnn <kflinncoding@gmail.com>
2022-11-15 19:56:36 -0500

    adding this week's files with activities 6 and 7 edited

---
+++ 03-JavaScript/01-Activities/06-Arrays/Unsolved/script.js
@@ -0,0 +1,16 @@
+// WRITE YOUR CODE HERE
+var names = ["Alex", "Kelley", "Shobannah", "Donovan", "Adrian"]
+
+console.log(names.length);
+
+console.log("Welcome to the class " + names[0])
+console.log("Welcome to the class " + names[1])
+console.log("Welcome to the class " + names[2])
+console.log("Welcome to the class " + names[3])
+console.log("Welcome to the class " + names[4])
+
+names[0] = "Kelley"
+
+if (students[0] === "Kelley") {
+    console.log(names[0])
+}
---
+++ 03-JavaScript/01-Activities/07-Iteration/Unsolved/script.js
@@ -0,0 +1,8 @@
+//WRITE YOUR CODE BELOW
+var students = ["Kelley", "Gustavo", "Michael", "Benjamin", "Shemar"];
+
+console.log(students.length);
+
+for (var i = 0; i < students.length; i++) {
+    console.log("Great to see you, " + students[i] + "!");
+}
