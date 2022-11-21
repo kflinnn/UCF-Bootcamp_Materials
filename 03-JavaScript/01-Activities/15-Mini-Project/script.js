cc8998c26e095d2da51da76a7d8cacd0bda97573
kflinnn <kflinncoding@gmail.com>
2022-11-21 18:34:33 -0500

    added this week's class materials, APIs

---
+++ 03-JavaScript/01-Activities/15-Mini-Project/script.js
@@ -0,0 +1,29 @@
+var Wins = 0;
+var Losses = 0;
+var Ties = 0;
+
+var options = ["r", "p", "s"];
+
+
+//Accept User's input
+prompt("Select R, P, or S"); 
+var userChoice
+//Computer has to choose R, P, or S
+
+
+//Compare -conditional statements- and declare a winner
+function delcareWinner (x,y) {
+if (r = s) {
+    console.log("You win!");
+}
+}
+
+
+//Display result
+
+
+//Alert with wins, ties, and losses
+
+
+//Prompt to play again
+confirm("Do you want to play again?");
