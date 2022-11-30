8253499ed2b460a2f4767950513514a22b7fe821
kflinnn <kflinncoding@gmail.com>
2022-11-29 21:26:16 -0500

    worked on Activities 1-5 during class

--- 05-Third-Party-APIs/01-Activities/03-Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/03-Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,14 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
+var input = $("#shopping-input");
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit (event) {
+    event.preventDefault ()
+
+    var inputVal = input.val ()
+
+}
 
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on("submit", handleFormSubmit);
