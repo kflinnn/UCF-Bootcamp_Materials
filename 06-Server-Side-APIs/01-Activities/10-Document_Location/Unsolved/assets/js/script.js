a036f1858b251d3c46ec3a41b26b9c067e9db4de
kflinnn <kflinncoding@gmail.com>
2022-12-08 21:10:52 -0500

    finished activity 10 in class

--- 06-Server-Side-APIs/01-Activities/10-Document_Location/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/10-Document_Location/Unsolved/assets/js/script.js
@@ -3,6 +3,8 @@ var redirectUrl = "./404.html";
 
 fetch(badRequestUrl).then(function (response) {
 	// Use a conditional to check the response status.
-	
+	if (response.status === 404){
+		location.replace(redirectUrl);
+	}
 	// If that status equals the conditional, then redirect to the 404 page.
 	});
