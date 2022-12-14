06ec77f834044166ed289fa2f7a19ed1c3a23e49
kflinnn <kflinncoding@gmail.com>
2022-12-14 16:24:35 -0500

    completed activities 1-5 of week 6

--- 06-Server-Side-APIs/01-Activities/04-Parse_JSON/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/04-Parse_JSON/Unsolved/assets/js/script.js
@@ -1,5 +1,7 @@
 // TODO: Edit the URL to get only 5 issues of Twitter's Chill repo
-var requestUrl = "https://api.github.com/repos/{owner}/{repo}/issues";
+var requestUrl = "https://api.github.com/repos/twitter/chill/issues?per_page=5";
+
+//^add ? after issues and then per_page to show you want to specify how many results you want, and then =5 for the number. 
 
 fetch(requestUrl)
 	.then(function (response) {
@@ -9,5 +11,10 @@ fetch(requestUrl)
 		console.log("Github Repo Issues \n----------");
 		console.log(data);
 		// TODO: Loop through the response
+		for (i = 0; i < data.length; i++){
+	
 		// TODO: Console log each issue's URL and each user's login
+		console.log(data[i].url); //this lists each URL out line by line
+		console.log(data[i].user.login); //this says who the user was for each issue
+	}
 	});
--- 06-Server-Side-APIs/01-Activities/05-Demo_Dynamic/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/05-Demo_Dynamic/Unsolved/assets/js/script.js
@@ -12,6 +12,17 @@ function getApi() {
 			// Use the console to examine the response
 			console.log(data);
 			// TODO: Loop through the data and generate your HTML
+		for (var i = 0; i < data.length; i++){
+			var userName = document.createElement("h3");
+			var userUrl = document.createElement ("p");
+			
+			userName.textContent = data[i].login;
+			userUrl.textContent = data[i].url;
+			
+			userContainer.append(userName);
+			userContainer.append(userUrl);
+		}
+		
 		});
 }
 fetchButton.addEventListener("click", getApi);
