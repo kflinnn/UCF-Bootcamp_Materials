9706ef2a8143c675f4a0e610c3b8971248e87879
kflinnn <kflinncoding@gmail.com>
2023-01-09 21:32:15 -0500

    updated activities 6-10 for week 9

--- 09-NodeJS/01-Activities/07-appendFile/Unsolved/index.js
+++ 09-NodeJS/01-Activities/07-appendFile/Unsolved/index.js
@@ -6,3 +6,4 @@ fs.appendFile('log.txt', `${process.argv[2]}\n`, (err) =>
   // TODO: Describe how this ternary operator works
   err ? console.error(err) : console.log('Commit logged!')
 );
+//if there is an error, console log error
