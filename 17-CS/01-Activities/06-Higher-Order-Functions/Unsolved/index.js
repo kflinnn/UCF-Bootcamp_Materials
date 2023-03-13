501c656927bcd948bc76e464e4e85315ed69e56b
kflinnn <kflinncoding@gmail.com>
2023-03-13 18:43:48 -0400

    added solved folders for classes 2 and 3 of week 17

--- 17-CS/01-Activities/06-Higher-Order-Functions/Unsolved/index.js
+++ 17-CS/01-Activities/06-Higher-Order-Functions/Unsolved/index.js
@@ -2,10 +2,14 @@ const grades = [56, 78, 99, 85];
 
 // TODO: Complete the 'findAverage' function below.
 function findAverage(accumulator, currentValue, index, array) {
-    
+    let total = 0;
+for(let i = 0; i < grades.length; i++) {
+    total += grades[i];
+}
+let avg = total / grades.length;
 }
 
 // TODO: Pass the 'findAverage' into the reduce method.
-let gradeAverage = grades.reduce();
+let gradeAverage = grades.reduce(findAverage);
 
 console.log(gradeAverage);
--- 17-CS/01-Activities/07-Closures/Unsolved/index.js
+++ 17-CS/01-Activities/07-Closures/Unsolved/index.js
@@ -1,4 +1,12 @@
 // TODO: Complete the 'counter' function below.
-function counter() {}
+function counter() {
+    const students = 10;
+
+    return {
+        displayCount: function () {
+            console.log(`You have $${students} students in your class`);
+        },
+    };
+}
 
 module.exports = counter;
