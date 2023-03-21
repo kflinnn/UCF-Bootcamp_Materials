1bbaba95209ecdad9d879a664536e301252f5200
kflinnn <kflinncoding@gmail.com>
2023-03-20 21:46:38 -0400

    added the remaining week 18 solved folders

--- 18-NoSQL/01-Activities/11-Virtuals/Unsolved/models/User.js
+++ 18-NoSQL/01-Activities/11-Virtuals/Unsolved/models/User.js
@@ -9,14 +9,25 @@ const userSchema = new Schema(
   {
     toJSON: {
       // TODO: Mongoose will not include virtuals by default, so add a `virtuals` property and set it's value to true
+      virtuals: true,
     },
     id: false,
   }
 );
 
 // TODO: Create a virtual property `fullName` on the userSchema
+userSchema.virtual('fullName').get(function () {
+    return this.first + ' ' + this.last
+}).set( function (v) {
+  const firstName = v.substring(0, v.indexOf(' '));
+  const lastName = v.substring(v.indexOf(' ' + 1));
+  this.set({ firstName, lastName });
+})
 
 // TODO: Create a getter for the virtual that returns the full name of the user (first + last)
+
+
+
 
 // TODO: Create a setter for the virtual that sets the value of the first and last name, given just the `fullName`
 
