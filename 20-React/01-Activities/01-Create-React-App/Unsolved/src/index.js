5b4992bf42cb3813728016bcb1457a162e6953cd
kflinnn <kflinncoding@gmail.com>
2023-03-28 21:23:17 -0400

    added react app, completed activities 1-5

--- 20-React/01-Activities/01-Create-React-App/Unsolved/src/index.js
+++ 20-React/01-Activities/01-Create-React-App/Unsolved/src/index.js
@@ -1,7 +1,9 @@
 // TODO: Add a comment giving a brief description of what React is
+// A front-end JavaScript library
 import React from 'react';
 
 // TODO: Add a comment that describes the difference between react and react-dom
+// 
 import ReactDOM from 'react-dom';
 
 import App from './App';
